b23e211eb221357c33fa661a7cec914305ef578d
root-kjh <wldnro3141@gmail.com>
2023-02-07 22:37:29 +0900

    feat: add exception logger

--- src/exception/exception.module.ts
+++ src/exception/exception.module.ts
@@ -1,4 +1,8 @@
-import { Module } from '@nestjs/common';
+import { Logger, Module } from '@nestjs/common';
+import { APP_FILTER } from '@nestjs/core';
+import { HttpExceptionFilter } from './http-exception.filter';
 
-@Module({})
+@Module({
+  providers: [Logger, { provide: APP_FILTER, useClass: HttpExceptionFilter }],
+})
 export class ExceptionModule {}
